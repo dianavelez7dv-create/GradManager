cdf107b687cab43221885b0bddd9a34c2888fdcc
dianavelez7dv-create <dianavelez7.dv@gmail.com>
2026-06-11 23:29:21 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,43 +1,20 @@
-// GradManager Service Worker v1
-const CACHE_NAME = 'gradmanager-v1';
-const urlsToCache = [
-  './GradManager_FINAL.html',
-];
+// GradManager Service Worker v128
+const CACHE_NAME = 'gradmanager-v128';
 
-// Install
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
   self.skipWaiting();
 });
 
-// Activate
 self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(keys) {
-      return Promise.all(
-        keys.filter(function(key) { return key !== CACHE_NAME; })
-            .map(function(key) { return caches.delete(key); })
-      );
-    })
+      return Promise.all(keys.map(function(key) { return caches.delete(key); }));
+    }).then(function(){ return self.clients.claim(); })
   );
-  self.clients.claim();
 });
 
-// Fetch - Network first, cache fallback
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).then(function(response) {
-      var responseClone = response.clone();
-      caches.open(CACHE_NAME).then(function(cache) {
-        cache.put(event.request, responseClone);
-      });
-      return response;
-    }).catch(function() {
-      return caches.match(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request).catch(function(){
+    return caches.match(event.request);
+  }));
 });
