3528ff50d01818c1aba33d5b19abb9eda66381e0
dianavelez7dv-create <dianavelez7.dv@gmail.com>
2026-07-05 09:37:16 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gradmanager-JULIO-317b';
+const CACHE_NAME = 'gradmanager-JULIO-318';
 self.addEventListener('install', function(event){ self.skipWaiting(); });
 self.addEventListener('activate', function(event){
   event.waitUntil(
